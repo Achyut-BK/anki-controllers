fb5af6734750a2419c8706151e01fc4598274e61
Achyut-BK <achyut.bharathkumar@gmail.com>
2022-03-12 16:29:47 +0530

    Added init_gamepad function

--- src/init.rs
+++ src/init.rs
@@ -1,5 +1,29 @@
-fn init_gamepad() -> Option<usize> {
-    unimplemented!();
+use gilrs::ev::{Button, EventType};
+use gilrs::{Event, Gilrs, MappingSource};
+use log::{info, warn};
+
+pub fn init_gamepad() -> Option<usize> {
+    let mut gilrs = Gilrs::new().unwrap();
+    let mut active_gamepad_id: Option<usize>;
+
+    loop {
+        if let Some(Event {
+            id,
+            event: EventType::ButtonPressed(Button::Mode, _),
+            time: _,
+        }) = gilrs.next_event()
+        {
+            match gilrs.gamepad(id).mapping_source() {
+                MappingSource::None => {
+                    warn!("This Gamepad is Unsupported. It may still work. Use at own Risk")
+                }
+                _ => info!("{} is supported, Proceeding", gilrs.gamepad(id).name()),
+            }
+            active_gamepad_id = Some(id.into());
+            break;
+        }
+    }
+    active_gamepad_id
 }
 
 use log::{LevelFilter, SetLoggerError};
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,10 @@
-use env_logger::Builder;
-use gilrs::ev::{Button, EventType};
-use gilrs::{Event, Gilrs, MappingSource};
 use log::LevelFilter;
-use log::{error, info, warn};
-use std::env;
-
 mod init;
 
 fn main() {
     //  Initialize logger
     init::init_log(Some(LevelFilter::Info));
-
-    // Create new gamepad access object
-    let mut gilrs = Gilrs::new().unwrap();
-    // Create active gamepad id
-    let mut active_gamepad_id: Option<usize>;
-
-    // Wait for Mode button press, test gamepad validity, and register gamepad
-    loop {
-        if let Some(Event {
-            id,
-            event: EventType::ButtonPressed(Button::Mode, _),
-            time: _,
-        }) = gilrs.next_event()
-        {
-            match gilrs.gamepad(id).mapping_source() {
-                MappingSource::None => {
-                    warn!("This Gamepad is Unsupported. It may still work. Use at own Risk")
-                }
-                _ => info!("{} is supported, Proceeding", gilrs.gamepad(id).name()),
-            }
-            active_gamepad_id = Some(id.into());
-            break;
-        }
-    }
-    //    let mut active_gamepad = None;
+    init::init_gamepad();
 }
 
 /*
